341fe0d4436f3e97ff12729299c2f4f23c7a936a
cibinoommen <cibin.p.oommen@gmail.com>
2017-07-29 15:56:08 +1000

    Login and Register Screens

--- MySmartCommunityApp/app/src/main/java/com/govhack/mysmartcommunityapp/MainActivity.java
+++ MySmartCommunityApp/app/src/main/java/com/govhack/mysmartcommunityapp/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.idea_platform) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.voln_platform) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.bus_enablers) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.infra_logistics) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.active_wellbeing) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.safety_security) {
 
         }
 
